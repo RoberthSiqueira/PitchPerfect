0985fcec20370c7d3145366693191327d840af8f
Roberth Siqueira <roberth.siqueira@gmail.com>
2023-08-17 22:24:27 -0300

    add view functions

--- PitchPerfect/ViewController.swift
+++ PitchPerfect/ViewController.swift
@@ -5,8 +5,15 @@ class ViewController: UIViewController {
   override func viewDidLoad() {
     super.viewDidLoad()
     view.backgroundColor = .white
+    addViewHierarchy()
+    setupConstraints()
   }
 
 
+  private func addViewHierarchy() {
 }
+  
+  private func setupConstraints() {
+  }
+  }
 
