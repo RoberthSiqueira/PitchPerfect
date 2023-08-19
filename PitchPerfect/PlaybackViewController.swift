15be848bfcfee7ca5d9a7ed8489bfbf449a67fc4
Roberth Siqueira <roberth.siqueira@gmail.com>
2023-08-18 22:35:32 -0300

    add audio options buttons

--- PitchPerfect/PlaybackViewController.swift
+++ PitchPerfect/PlaybackViewController.swift
@@ -5,12 +5,81 @@ class PlaybackViewController: UIViewController {
     var audioRecorderURL: URL
 
     private lazy var playbackOptionsStackView: UIStackView = {
-        let stackView = UIStackView(frame: .zero)
+        let stackView = UIStackView(arrangedSubviews: [playbackLineStackView])
         stackView.translatesAutoresizingMaskIntoConstraints = false
         stackView.axis = .vertical
         stackView.distribution = .fill
         stackView.spacing = .zero
         return stackView
+    }()
+
+    private lazy var playbackLineStackView: UIStackView = {
+        let stackView = UIStackView(arrangedSubviews: [slowAudioButton, fastAudioButton])
+        stackView.translatesAutoresizingMaskIntoConstraints = false
+        stackView.axis = .horizontal
+        stackView.distribution = .fillEqually
+        stackView.spacing = .zero
+        return stackView
+    }()
+
+    private lazy var slowAudioButton: UIButton = {
+        let button = UIButton(frame: .zero)
+        button.translatesAutoresizingMaskIntoConstraints = false
+        button.setImage(UIImage(imageLiteralResourceName: "Slow"), for: .normal)
+//        button.addTarget(self, action: #selector(), for: .touchUpInside)
+        return button
+    }()
+
+    private lazy var fastAudioButton: UIButton = {
+        let button = UIButton(frame: .zero)
+        button.setImage(UIImage(imageLiteralResourceName: "Fast"), for: .normal)
+        return button
+    }()
+
+    private lazy var distanceLineStackView: UIStackView = {
+        let stackView = UIStackView(arrangedSubviews: [highAudioButton, lowAudioButton])
+        stackView.axis = .horizontal
+        stackView.alignment = .fill
+        stackView.distribution = .fillEqually
+        stackView.spacing = .zero
+        return stackView
+    }()
+
+    private lazy var highAudioButton: UIButton = {
+        let button = UIButton(frame: .zero)
+        button.setImage(UIImage(imageLiteralResourceName: "HighPitch"), for: .normal)
+        return button
+    }()
+
+    private lazy var lowAudioButton: UIButton = {
+        let button = UIButton(frame: .zero)
+        button.setImage(UIImage(imageLiteralResourceName: "LowPitch"), for: .normal)
+        return button
+    }()
+
+    private lazy var reflectionLineStackView: UIStackView = {
+        let stackView = UIStackView(arrangedSubviews: [echoAudioButton, reverbAudioButton])
+        stackView.axis = .horizontal
+        stackView.alignment = .fill
+        stackView.distribution = .fillEqually
+        stackView.spacing = .zero
+        return stackView
+    }()
+
+    private lazy var echoAudioButton: UIButton = {
+        let button = UIButton(frame: .zero)
+        button.setImage(UIImage(imageLiteralResourceName: "Echo"), for: .normal)
+        return button
+    }()
+
+    private lazy var reverbAudioButton: UIButton = {
+        let button = UIButton(frame: .zero)
+        button.setImage(UIImage(imageLiteralResourceName: "Reverb"), for: .normal)
+        return button
+    }()
+
+//        button.addTarget(self, action: #selector(), for: .touchUpInside)
+        return button
     }()
 
     init(audioRecorderURL: URL) {
@@ -35,6 +104,14 @@ class PlaybackViewController: UIViewController {
     }
 
     private func setupConstraints() {
+        let safeAreaLayoutGuide = view.safeAreaLayoutGuide
+
+        NSLayoutConstraint.activate([
+            playbackOptionsStackView.topAnchor.constraint(equalTo: safeAreaLayoutGuide.topAnchor),
+            playbackOptionsStackView.leadingAnchor.constraint(equalTo: safeAreaLayoutGuide.leadingAnchor, constant: 8 ),
+            playbackOptionsStackView.trailingAnchor.constraint(equalTo: safeAreaLayoutGuide.trailingAnchor, constant: -8)
+        ])
+
         NSLayoutConstraint.activate([
             playbackOptionsStackView.topAnchor.constraint(equalTo: view.topAnchor),
             playbackOptionsStackView.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor, constant: 8 ),
